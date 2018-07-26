122a5bc2f7321819e60237b5a3c3325323ee4381
Paulo Moura <paulociecomp@gmail.com>
2018-07-25 23:45:40 -0300

    tick controller logic

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,19 @@
 class ItemsController < ApplicationController
   def tick
-    render json: params[:items]
+    items = []
+
+    items_params.each do |param|
+      item = Item.new(param)
+      item.tick
+      items << item
+    end
+
+    render json: items
+  end
+
+  private
+
+  def items_params
+    JSON.parse(request.raw_post, symbolize_names: true)[:items]
   end
 end
--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,14 +1,25 @@
 require "rails_helper"
 
 RSpec.describe ItemsController, type: :request do
+  let(:header){ { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' } }
+
   def body
-    JSON.parse(response.body)
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   it "ticks an empty list" do
     post "/items/tick",
       params: { items: [] }.to_json,
-      headers: { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+      headers: header
     expect(body).to eq []
+  end
+
+  it "tick items" do
+    items = [ { "type": "Normal Item", "days_remaining": 5, "quality": 10 } ]
+    post "/items/tick",
+      params: { items: items }.to_json,
+      headers: header
+
+    expect(body[0][:days_remaining]).to eq 4
   end
 end
