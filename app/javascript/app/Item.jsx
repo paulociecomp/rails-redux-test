5394ca197f3e4c8fab62343a6d8668d0f4f2df3e
Paulo Moura <paulociecomp@gmail.com>
2018-07-27 03:32:13 -0300

    css to adjust layout

--- app/javascript/app/Item.jsx
+++ app/javascript/app/Item.jsx
@@ -2,10 +2,16 @@ import React from "react";
 
 const Item = ({ item }) => (
 	<div className="item">
-		{item.id}
-		{item.type}
-		Quality: {item.quality}
-		Days remaining: {item.days_remaining}
+		<div className="item-left">
+			<span className="item-id">#{item.id}</span>
+			<h2 className="item-type">{item.type}</h2>
+		</div>
+
+		<div className="item-rigth">
+			<p>Quality: {item.quality}</p>
+			<p>Days remaining: {item.days_remaining}</p>
+		</div>
+		<div className="clearfix" />
 	</div>
 );
 
--- app/javascript/app/Navbar.jsx
+++ app/javascript/app/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = ({ daysElapsed }) => (
-  <nav className="navbar">
-    <h1 className="logo">Stock</h1>
-
-    <span className="days-elapsed">Days elapsed: {daysElapsed}</span>
-  </nav>
+	<nav className="navbar">
+		<div className="container">
+			<h1 className="logo">Stock</h1>
+			<span className="days-elapsed">Days elapsed: {daysElapsed}</span>
+			<div className="clearfix" />
+		</div>
+	</nav>
 );
 
 export default Navbar;
